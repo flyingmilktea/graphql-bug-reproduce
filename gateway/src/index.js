401388fcf302b38a9db53e9146df4e16329611bd
kftse <kftse20031207@gmail.com>
2022-06-06 03:08:31 +0800

    did not fix

--- gateway/src/index.js
+++ gateway/src/index.js
@@ -5,38 +5,31 @@ const executableSchema = require("./executable-schema");
 const { introspectRemoteSchemas } = require("./remote-schema");
 
 const schemaFn = async () => {
-  let mergedSchema = executableSchema;
-
-  if (!(process.env.DISABLE_REMOTE?.toLowerCase() === "true")) {
-    mergedSchema = stitchSchemas({
-      subschemas: [
-        {
-          schema: await introspectRemoteSchemas(["/server1"]),
-          merge: {
-            User: {
-              fieldName: "oneUser",
-              selectionSet: "{ id }",
-              args: (o) => ({ id: o.id }),
-            },
+  const mergedSchema = stitchSchemas({
+    subschemas: [
+      {
+        schema: executableSchema,
+        merge: {
+          User: {
+            fieldName: "zeroUser",
+            selectionSet: "{ id }",
+            args: (o) => ({ id: o.id }),
           },
         },
-        {
-          schema: executableSchema,
-          merge: {
-            User: {
-              fieldName: "zeroUser",
-              selectionSet: "{ id }",
-              args: (o) => ({ id: o.id }),
-            },
-          },
-        },
-      ],
-      mergeTypes: true, // << default in v7
-      typeMergingOptions: {
-        fieldConfigMerger: (candidates) => candidates[0].fieldConfig,
       },
-    });
-  }
+      {
+        schema: await introspectRemoteSchemas(["/server1"]),
+        merge: {
+          User: {
+            fieldName: "oneUser",
+            selectionSet: "{ id }",
+            args: (o) => ({ id: o.id }),
+          },
+        },
+      },
+    ],
+    mergeTypes: true, // << default in v7
+  });
 
   return mergedSchema;
 };
